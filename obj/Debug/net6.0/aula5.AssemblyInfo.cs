73bb4e0ddbbaed33c7f4ec1bc3f7be9595a01833
matheusgriffovasconcelos <matheus.griffo.vasconcelos@gmail.com>
2022-10-26 10:43:06 -0300

    alteracao banco

--- obj/Debug/net6.0/aula5.AssemblyInfo.cs
+++ obj/Debug/net6.0/aula5.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("aula5")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Aula5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("aula5")]
-[assembly: System.Reflection.AssemblyTitleAttribute("aula5")]
+[assembly: System.Reflection.AssemblyProductAttribute("Aula5")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Aula5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
